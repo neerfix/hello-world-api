cb245553c5d4f50a8cc4f76cf7cc4b55f7ca1c4b
Hikari-rom <romainfpro@gmail.com>
2022-02-15 19:17:00 +0100

    feat: :sparkles: add File & FileRepository

---
+++ src/Entity/File.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FileRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FileRepository::class)]
+class File
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $extension;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $created_at;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $updated_at;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'files')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $user_id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $uuid;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $type;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $mime_type;
+
+    #[ORM\Column(type: 'integer')]
+    private $size;
+
+    #[ORM\ManyToMany(targetEntity: AlbumFile::class, mappedBy: 'file_id')]
+    private $albumFiles;
+
+    public function __construct()
+    {
+        $this->albumFiles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getExtension(): ?string
+    {
+        return $this->extension;
+    }
+
+    public function setExtension(?string $extension): self
+    {
+        $this->extension = $extension;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?User $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getUuid(): ?string
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid(string $uuid): self
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getMimeType(): ?string
+    {
+        return $this->mime_type;
+    }
+
+    public function setMimeType(?string $mime_type): self
+    {
+        $this->mime_type = $mime_type;
+
+        return $this;
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->size;
+    }
+
+    public function setSize(int $size): self
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|AlbumFile[]
+     */
+    public function getAlbumFiles(): Collection
+    {
+        return $this->albumFiles;
+    }
+
+    public function addAlbumFile(AlbumFile $albumFile): self
+    {
+        if (!$this->albumFiles->contains($albumFile)) {
+            $this->albumFiles[] = $albumFile;
+            $albumFile->addFileId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAlbumFile(AlbumFile $albumFile): self
+    {
+        if ($this->albumFiles->removeElement($albumFile)) {
+            $albumFile->removeFileId($this);
+        }
+
+        return $this;
+    }
+}
