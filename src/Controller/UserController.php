27c419c12779b262fca058f9619fcb6993a18a05
MLivain <matisse.livain@gmail.com>
2022-02-18 11:45:45 +0100

    feat: CRUD User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,7 @@ use App\Services\RequestService;
 use App\Services\ResponseService;
 use Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
@@ -56,14 +57,140 @@ class UserController extends HelloworldController
     public function getAllAction(): JsonResponse
     {
         $loggedUser = $this->getLoggedUser();
-
+        $roles = $loggedUser->getRoles();
         // No logged used
-//        if (empty($loggedUser)) {
-//            return $this->responseService->error403('auth.unauthorized', 'Vous n\'êtes pas autorisé à effectué cette action');
-//        }
+        if (empty($loggedUser) && in_array("ADMIN",$roles)) {
+            return $this->responseService->error403('auth.unauthorized', 'Vous n\'êtes pas autorisé à effectué cette action');
+        }
 
         $users = $this->userRepository->findAll();
 
         return $this->buildSuccessResponse(Response::HTTP_OK, $users, $loggedUser);
     }
+
+    /**
+     * @Route("/users/signup", name="auth_signup_email", methods={ "POST" })
+     *
+     * @throws NonUniqueResultException
+     * @throws ORMException
+     * @throws OptimisticLockException
+     * @throws WorkingException
+     * @throws ExceptionInterface
+     * @throws Exception
+     */
+    public function userSignup(Request $request): JsonResponse
+    {
+        $errors = $this->validate($request->request->all(), [
+            'email' => [new Email(), new NotBlank()],
+            'password' => [new Type(['type' => 'string']), new NotBlank()],
+            'birthDate' => [new DateTime(['format' => 'Y-m-d']), new NotBlank()],
+            'userName' => [new Type(['type' => 'string'])],
+            'firstName' => [new Optional([new Type(['type' => 'string'])])],
+            'lastName' => [new Optional([new Type(['type' => 'string'])])],
+        ]);
+
+        // Validation errors
+        if (!empty($errors)) {
+            return $errors;
+        }
+
+        $birthDate = $this->getDate($request, $request->request->get('birthDate'));
+
+        $user = $this->userService->create(
+            $request->request->get('email'),
+            $request->request->get('password'),
+            $request->request->get('userName'),
+            $birthDate,
+            $request->request->get('firstName'),
+            $request->request->get('lastName'),
+        );
+
+        return $this->buildSuccessResponse(Response::HTTP_CREATED, $user, $user);
+    }
+
+    /**
+     * @Route("/users/{id}/delete", name="delete_user",methods={"DELETE"})
+     */
+    public function deleteUsers($id,UserRepository $repo,  EntityManagerInterface $manager){
+        $loggedUser = $this->getLoggedUser();
+        $roles = $loggedUser->getRoles();
+        // No logged used
+        if (empty($loggedUser) || in_array("ADMIN",$roles)) {
+            return $this->responseService->error403('auth.unauthorized', 'Vous n\'êtes pas autorisé à effectué cette action');
+        }
+
+        $exists = 'no';
+        if($user = $repo->find($id)) {
+            $exist = 'yes';
+            $manager->remove($user);
+            $manager->flush();
+        }
+        $response = new Response('{ "id" : '. $id .' '. $exist.'  }');
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
+    }
+
+    /**
+     * @Route("/users/update", name="user_update, methods={ "POST" })
+     *
+     * @throws NonUniqueResultException
+     * @throws ORMException
+     * @throws OptimisticLockException
+     * @throws WorkingException
+     * @throws ExceptionInterface
+     * @throws Exception
+     */
+    public function userUpdate(Request $request,UserRepository $repo,  EntityManagerInterface $manager): JsonResponse
+    {
+        $loggedUser = $this->getLoggedUser();
+
+        $roles = $loggedUser->getRoles();
+
+        // No logged used
+        if (empty($loggedUser) || in_array("ADMIN",$roles)) {
+            return $this->responseService->error403('auth.unauthorized', 'Vous n\'êtes pas autorisé à effectué cette action');
+        }
+
+        $errors = $this->validate($request->request->all(), [
+            'id' => [new Uuid()],
+            'email' => [new Email(), new NotBlank()],
+            'password' => [new Type(['type' => 'string']), new NotBlank()],
+            'birthDate' => [new DateTime(['format' => 'Y-m-d']), new NotBlank()],
+            'userName' => [new Type(['type' => 'string'])],
+            'firstName' => [new Optional([new Type(['type' => 'string'])])],
+            'lastName' => [new Optional([new Type(['type' => 'string'])])],
+            'isVerify' => [new Type(['type' => 'bool'])]
+        ]);
+
+        // Validation errors
+        if (!empty($errors)) {
+            return $errors;
+        }
+
+        $exists = 'no';
+
+        $id = $request->request->get('id');
+
+        $birthDate = $this->getDate($request, $request->request->get('birthDate'));
+
+        $user = $repo->find($id);
+        if($user) {
+            $exist = 'yes';
+            $user->setEmail($request->request->get('email'));
+            $user->setPassword($request->request->get('password'));
+            $user->setUsername($request->request->get('userName'));
+            $user->setDateOfBirth($birthDate);
+            $user->setFirstname($request->request->get('firstName'));
+            $user->setLastname($request->request->get('lastName'));
+            $user->setIsVerify($request->request->get('isVerify'));
+        }
+
+        $response = new Response('{ "id" : '. $id .' '. $exist.'  }');
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        return $this->buildSuccessResponse(Response::HTTP_ACCEPTED, $user, $user);
+    }
+
 }
