c1d5342bcf08b5d964b7f8c259d0af2d7456c997
Nicolas Notararigo <nicolas.notararigo@ynov.com>
2022-02-24 17:26:48 +0100

    feat(sentry): add sentry to project

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
