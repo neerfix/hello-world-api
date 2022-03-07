0904487697414e9c5bbe36486e3af0946a49a5f8
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2022-03-07 12:35:09 +0100

    fix: loginAuthenticator

--- src/Controller/Auth/LoginController.php
+++ src/Controller/Auth/LoginController.php
@@ -4,22 +4,13 @@ namespace App\Controller\Auth;
 
 use App\Controller\HelloworldController;
 use App\Entity\User;
-use App\Security\ApiKeyAuthenticator;
-use App\Services\LoginService;
 use App\Services\RequestService;
 use App\Services\ResponseService;
-use App\Services\SecurityService;
-use App\Services\UserService;
 use Exception;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-use Symfony\Component\Validator\Constraints\DateTime;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Optional;
-use Symfony\Component\Validator\Constraints\Type;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class LoginController extends HelloworldController
@@ -27,13 +18,12 @@ class LoginController extends HelloworldController
     // ------------------------ >
 
     public function __construct(
-        ResponseService $responseService,
-        RequestService $requestService,
-        ValidatorInterface $validator,
-        NormalizerInterface $normalizer,
-        private LoginService $loginService,
-        private UserService $userService
-    ) {
+        ResponseService      $responseService,
+        RequestService       $requestService,
+        ValidatorInterface   $validator,
+        NormalizerInterface  $normalizer,
+    )
+    {
         parent::__construct($responseService, $requestService, $validator, $normalizer);
     }
 
@@ -43,32 +33,14 @@ class LoginController extends HelloworldController
      * @Route("/auth/login", name="login", methods={ "POST" })
      *
      * @throws Exception
-     * @throws ExceptionInterface
      */
-    public function loginAction(?User $user): Response
+    public function index(#[CurrentUser] ?User $user): Response
     {
-//        $content = $request->getContent();
-//        $parameters = json_decode($content, true);
-//        $apiAuthenticator = new ApiKeyAuthenticator();
-//        $errors = $this->validate($parameters, [
-//            'email' => [new Type(['type' => 'string']), new NotBlank()],
-//            'password' => [new Type(['type' => 'string']), new NotBlank()],
-//        ]);
-//
-//        if (!empty($errors)) {
-//            return $errors;
-//        }
-//        $testPassport = $apiAuthenticator->authenticate($request);
-//        dump($testPassport);
-//        $user = $this->userService->login($parameters["email"],$parameters["password"]);
-//
-//        $login = $this->loginService->create($user, "V1", $request->getClientIp(), $request->headers->get('User-Agent'), true);
-
         if (null === $user) {
             return $this->json([
-                   'message' => 'missing credentials',
+                'message' => 'missing credentials',
             ], Response::HTTP_UNAUTHORIZED);
-         }
+        }
 
         $token = "Token-test";
 
@@ -76,7 +48,5 @@ class LoginController extends HelloworldController
             'user' => $user->getUserIdentifier(),
             'token' => $token
         ]);
-
-//        return $this->buildSuccessResponse(Response::HTTP_CREATED, );
     }
 }
