01c41dfc2f383e1acfcd8351522f4b10773d1b84
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2022-03-07 14:45:38 +0100

    feat: add jwt

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
