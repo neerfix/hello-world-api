3e9c003fda08e0c674122d87a3bb29fda1099721
Hikari-rom <romainfpro@gmail.com>
2022-03-09 20:16:51 +0100

    feat :sparkles: add migrations for uuid

--- src/Entity/Place.php
+++ src/Entity/Place.php
@@ -9,6 +9,7 @@ use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Table(name="place", indexes={
