34e3490fcf9a803118af786065ea2e1978b9de4c
Hikari-rom <romainfpro@gmail.com>
2022-03-10 15:02:16 +0100

    feat :sparkles: Add FileController & File Service

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -3,18 +3,13 @@
 namespace App\Controller;
 
 use App\Repository\FileRepository;
-use App\Repository\UserRepository;
 use App\Services\FileService;
 use App\Services\RequestService;
-use App\Services\ResponseService;
 use Symfony\Component\HttpFoundation\Request;
+use App\Services\ResponseService;
+use Ramsey\Uuid\Validator\ValidatorInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Exception\ExceptionInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Type;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class FileController extends HelloworldController
 {
@@ -22,41 +17,26 @@ class FileController extends HelloworldController
         ResponseService $responseService,
         RequestService $requestService,
         ValidatorInterface $validator,
-        NormalizerInterface $normalizer,
+        private NormalizerInterface $normalizer,
         private FileService $fileService,
-        private FileRepository $fileRepository,
-        private UserRepository $userRepository
-    ) {
+        private FileRepository $fileRepository
+    ){
         parent::__construct($responseService, $requestService, $validator, $normalizer);
     }
 
     /**
-     * @Route("/files", name="create_file", methods={"POST"})
-     *
-     * @throws Exception
+     * @Route("/files", name="create_file", methods={"POST})
      * @throws ExceptionInterface
+     * @throws Exception
      */
     public function addAction(Request $request): Response
     {
         $parameters = $this->getContent($request);
         $loggedUser = $this->getLoggedUser();
 
-        if (null === $loggedUser) {
+        if(null === $loggedUser) {
             return $this->buildErrorResponse(Response::HTTP_FORBIDDEN, 'auth.unauthorized', 'Vous n\'êtes pas autorisé à effectuer cette action');
         }
-
-        $errors = $this->validate($parameters, [
-            'path' => [new Type(['type' => 'string']), new NotBlank()],
-        ]);
-
-        if (!empty($errors)) {
-            return $errors;
-        }
-
-        $file = $this->fileService->create(
-            $parameters['path']
-        );
-
-        return $this->buildSuccessResponse(Response::HTTP_CREATED, $file, $loggedUser);
+        return $this->buildSuccessResponse(Response::HTTP_CREATED,"truc", $loggedUser);
     }
 }
--- src/Services/FileService.php
+++ src/Services/FileService.php
@@ -2,29 +2,14 @@
 
 namespace App\Services;
 
-use App\Entity\File;
+use App\Repository\FileRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Ramsey\Uuid\Uuid;
 
 class FileService
 {
     public function __construct(
-        private EntityManagerInterface $em,
-    ) {
-    }
+        EntityManagerInterface $em,
+        private FileRepository $fileRepository
+    ){}
 
-    public function create(string $path): File
-    {
-        $path = trim($path);
-
-        $file = (new File())
-            ->setPath($path)
-            ->setStatus(File::STATUS_ACTIVE)
-            ->setUuid(Uuid::uuid4());
-
-        $this->em->persist($file);
-        $this->em->flush();
-
-        return $file;
-    }
 }
