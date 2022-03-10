a89db7381af24930aabba50e236e5d4e7016851e
Hikari-rom <romainfpro@gmail.com>
2022-03-10 19:00:30 +0100

    feat :sparkles: Add Get 1 By Uuid & Get All for Files

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -2,14 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\File;
 use App\Repository\FileRepository;
+use App\Repository\UserRepository;
 use App\Services\FileService;
 use App\Services\RequestService;
-use Symfony\Component\HttpFoundation\Request;
 use App\Services\ResponseService;
-use Ramsey\Uuid\Validator\ValidatorInterface;
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Type;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class FileController extends HelloworldController
 {
@@ -17,26 +24,77 @@ class FileController extends HelloworldController
         ResponseService $responseService,
         RequestService $requestService,
         ValidatorInterface $validator,
-        private NormalizerInterface $normalizer,
+        NormalizerInterface $normalizer,
         private FileService $fileService,
-        private FileRepository $fileRepository
-    ){
+        private FileRepository $fileRepository,
+        private UserRepository $userRepository
+    ) {
         parent::__construct($responseService, $requestService, $validator, $normalizer);
     }
 
     /**
-     * @Route("/files", name="create_file", methods={"POST})
-     * @throws ExceptionInterface
+     * @Route("/files", name="create_file", methods={"POST"})
+     *
      * @throws Exception
+     * @throws ExceptionInterface
      */
     public function addAction(Request $request): Response
     {
         $parameters = $this->getContent($request);
         $loggedUser = $this->getLoggedUser();
 
-        if(null === $loggedUser) {
+        if (null === $loggedUser) {
             return $this->buildErrorResponse(Response::HTTP_FORBIDDEN, 'auth.unauthorized', 'Vous n\'êtes pas autorisé à effectuer cette action');
         }
-        return $this->buildSuccessResponse(Response::HTTP_CREATED,"truc", $loggedUser);
+
+        $errors = $this->validate($parameters, [
+            'path' => [new Type(['type' => 'string']), new NotBlank()],
+        ]);
+
+        if (!empty($errors)) {
+            return $errors;
+        }
+
+        $file = $this->fileService->create(
+            $parameters['path'],
+            $loggedUser
+        );
+
+        return $this->buildSuccessResponse(Response::HTTP_CREATED, $file, $loggedUser, ['groups' => ['file:read']]);
+    }
+
+    /**
+     * @Route("/files/{uuid}", name="get_one_file", methods={ "GET"})
+     *
+     * @throws Exception
+     * @throws ExceptionInterface
+     */
+    public function getAction(Request $request, string $uuid): Response
+    {
+        $loggedUser = $this->getLoggedUser();
+
+        if (null === $loggedUser) {
+            return $this->buildErrorResponse(Response::HTTP_FORBIDDEN, 'auth.unauthorized', 'Vous n\'êtes pas autorisé à effectuer cette action');
+        }
+        $file = $this->fileRepository->getOneByStatus($uuid, File::STATUS_ACTIVE);
+        if (null === $file) {
+            return $this->buildErrorResponse(Response::HTTP_NOT_FOUND, 'not.found', 'Le fichier n\'a pas été trouvé');
+        }
+
+        return $this->buildSuccessResponse(Response::HTTP_OK, $file, $loggedUser, ['groups' => ['file:read']]);
+    }
+
+    /**
+     * @Route("/files", name="get_all_files", methods={"GET"})
+     *
+     * @throws Exception
+     * @throws ExceptionInterface
+     */
+    public function getAllAction(Request $request): Response
+    {
+        $loggedUser = $this->getLoggedUser();
+        $files = $this->fileRepository->getAllByStatus(File::STATUS_ACTIVE);
+
+        return $this->buildSuccessResponse(Response::HTTP_OK, $files, $loggedUser, ['groups' => ['file:read']]);
     }
 }
--- src/Services/FileService.php
+++ src/Services/FileService.php
@@ -2,14 +2,31 @@
 
 namespace App\Services;
 
-use App\Repository\FileRepository;
+use App\Entity\File;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Ramsey\Uuid\Uuid;
 
 class FileService
 {
     public function __construct(
-        EntityManagerInterface $em,
-        private FileRepository $fileRepository
-    ){}
+        private EntityManagerInterface $em,
+    ) {
+    }
 
+    public function create(string $path, User $user): File
+    {
+        $path = trim($path);
+
+        $file = (new File())
+            ->setPath($path)
+            ->setStatus(File::STATUS_ACTIVE)
+            ->setUuid(Uuid::uuid4())
+            ->setUserId($user);
+
+        $this->em->persist($file);
+        $this->em->flush();
+
+        return $file;
+    }
 }
