d9a4b372d8de0c0c8883d9142aa1b48947519cb6
Hikari-rom <romainfpro@gmail.com>
2022-03-10 20:21:33 +0100

    fix :butterfly: clean migration

--- migrations/Version20220310155818.php
+++ migrations/Version20220310155818.php
@@ -21,21 +21,13 @@ final class Version20220310155818 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE UNIQUE INDEX UNIQ_2D4B9BCED17F51A8 ON file (uuid);');
-        $this->addSql('ALTER TABLE file DROP extension;');
-        $this->addSql('ALTER TABLE file DROP type;');
-        $this->addSql('ALTER TABLE file DROP mime_type;');
-        $this->addSql('ALTER TABLE file DROP size;');
-        $this->addSql('ALTER TABLE file ADD path VARCHAR(255) NOT NULL;');
+        $this->addSql('ALTER TABLE file DROP extension, DROP type, DROP mime_type, DROP size, ADD path VARCHAR(255) NOT NULL;');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP INDEX UNIQ_2D4B9BCED17F51A8 ON file;');
-        $this->addSql('ALTER TABLE file ADD extension VARCHAR(255) NOT NULL;');
-        $this->addSql('ALTER TABLE file ADD type VARCHAR(255) NOT NULL;');
-        $this->addSql('ALTER TABLE file ADD mime_type VARCHAR(255) NOT NULL;');
-        $this->addSql('ALTER TABLE file ADD size VARCHAR(255) NOT NULL;');
-        $this->addSql('ALTER TABLE file DROP path;');
+        $this->addSql('ALTER TABLE file ADD extension VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, mime_type VARCHAR(255) NOT NULL, size VARCHAR(255) NOT NULL, DROP path;');
     }
 }
